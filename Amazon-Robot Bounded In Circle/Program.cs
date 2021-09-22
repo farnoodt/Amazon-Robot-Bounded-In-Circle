b2fd7e99b60f2817cdac8b811eeceda4ad519a56
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-09-22 00:06:56 -0700

    Add project files.

---
+++ Amazon-Robot Bounded In Circle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Amazon_Robot_Bounded_In_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsRobotBounded("GGLLGG"));
+        }
+
+        static Boolean IsRobotBounded(string instructions)
+        {
+            int[] pos = new int[] { 0, 0 };
+
+            int[,] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+            int face = 0;
+            foreach (var c in instructions)
+            {
+                if (c == 'L')
+                    face = face == 0 ? 3 : face - 1;
+                else if (c == 'R')
+                    face = face == 3 ? 0 : face + 1;
+                else
+                {
+                    pos[0] = pos[0] + dirs[face, 0];
+                    pos[1] = pos[1] + dirs[face, 1];
+                }
+            }
+            return (face != 00 || (pos[0] == 0 && pos[1] == 0));
+        }
+    }
+}
